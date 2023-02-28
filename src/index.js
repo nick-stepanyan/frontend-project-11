2344c4e4ea07b796ca2a600f60144b2855fe9575
nick-stepanyan <nick.win.32@mail.ru>
2023-02-28 13:54:54 +0300

    problem with validate form

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import 'bootstrap';
 // import app from './app.js';
 // app();
 
-
 // import _ from 'lodash';
 import * as yup from 'yup';
 import onChange from 'on-change';
@@ -16,55 +15,55 @@ const error = document.querySelector('#error-input');
 const feeds = document.querySelector('.feeds');
 
 const schema = yup.object().shape({
-    url: yup
-        .string()
-        .url('Введите корректную ссылку')
-        .notOneOf(
-            [...feeds.querySelectorAll('a')].map((feed) => feed.href),
-            'Такая ссылка уже добавлена',
-        )
-        .required('Введите ссылку'),
+  url: yup
+    .string()
+    .url('Введите корректную ссылку')
+    .notOneOf(
+      [...feeds.querySelectorAll('a')].map((feed) => feed.href),
+      'Такая ссылка уже добавлена',
+    )
+    .required('Введите ссылку'),
 });
 
 const validate = (value) => {
-    schema
-        .validate({ url: value })
-        .then(() => {
-            input.classList.remove('is-invalid');
-            error.textContent = '';
-            form.reportValidity();
-        })
-        .catch((err) => {
-            input.classList.add('is-invalid');
-            error.textContent = err.message;
-            form.reportValidity();
-        });
+  schema
+    .validate({ url: value })
+    .then(() => {
+      input.classList.remove('is-invalid');
+      error.textContent = '';
+      form.reportValidity();
+    })
+    .catch((err) => {
+      input.classList.add('is-invalid');
+      error.textContent = err.message;
+      form.reportValidity();
+    });
 };
 
 onChange(input, (value) => {
-    validate(value);
+  validate(value);
 });
 
 form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    schema
-        .validate({ url: input.value })
-        .then(() => {
-            const feed = document.createElement('a');
-            feed.href = input.value;
-            feed.target = '_blank';
-            feed.rel = 'noopener noreferrer';
-            feed.classList.add('list-group-item', 'list-group-item-action');
-            feed.textContent = 'Загрузка...';
-            feeds.prepend(feed);
-            input.value = '';
-            input.focus();
-            validate('');
-            form.classList.remove('was-validated');
-        })
-        .catch((err) => {
-            input.classList.add('is-invalid');
-            error.textContent = err.message;
-            form.reportValidity();
-        });
+  event.preventDefault();
+  schema
+    .validate({ url: input.value })
+    .then(() => {
+      const feed = document.createElement('a');
+      feed.href = input.value;
+      feed.target = '_blank';
+      feed.rel = 'noopener noreferrer';
+      feed.classList.add('list-group-item', 'list-group-item-action');
+      feed.textContent = 'Загрузка...';
+      feeds.prepend(feed);
+      input.value = '';
+      input.focus();
+      validate('');
+      form.classList.remove('was-validated');
+    })
+    .catch((err) => {
+      input.classList.add('is-invalid');
+      error.textContent = err.message;
+      form.reportValidity();
+    });
 });
